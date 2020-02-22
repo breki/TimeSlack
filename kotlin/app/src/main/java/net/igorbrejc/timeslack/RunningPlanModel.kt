d205e8b4ec324a5b9e547ba7c5bab7f9019c56d8
Igor Brejc <igor.brejc@gmail.com>
2020-02-22 05:59:11 +0100

    misc

--- kotlin/app/src/main/java/net/igorbrejc/timeslack/RunningPlanModel.kt
+++ kotlin/app/src/main/java/net/igorbrejc/timeslack/RunningPlanModel.kt
@@ -1,16 +1,24 @@
 package net.igorbrejc.timeslack
 
+import kotlin.math.max
+
 data class RunningPlanModel(
     val plan: SlackerPlan,
     val activitiesLog: SlackerActivitiesLog,
     val currentTime: SlackerTime
 ) {
     fun planFinishTime(): SlackerTime {
-        currentTime.diffFrom(activitiesLog.currentActivityStartTime())
+        // if the current activity is running longer than the allotted time,
+        // use the actual running time instead
+        val currentActivityRunningTimeInMinutes =
+            max(
+                currentTime.diffFrom(activitiesLog.currentActivityStartTime()),
+                currentActivity().durationInMinutes)
+
 
         val totalDurationInMinutes =
-            currentActivity().durationInMinutes
-            + remainingActivities().sumBy { it.durationInMinutes }
+            (currentActivityRunningTimeInMinutes
+            + remainingActivities().sumBy { it.durationInMinutes })
 
         return activitiesLog.currentActivityStartTime()
             .add(totalDurationInMinutes)
