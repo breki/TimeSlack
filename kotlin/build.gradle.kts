81764855ecab333f1a70e3a4d08a9f9453677a05
Igor Brejc <igor.brejc@gmail.com>
2020-02-28 09:08:27 +0100

    Upgraded to com.android.tools.build:gradle:3.6.0

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         val kotlinVersion = "1.3.61"
         extra["kotlinVersion"] = kotlinVersion
 
-        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("com.android.tools.build:gradle:3.6.0")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
